697a7a3114830d4f81953ce640f21d6520e8eac7
Mister Diallo <77695206+misterdiallo@users.noreply.github.com>
2022-11-05 04:34:00 +0800

    Initial spring gradle project

---
+++ src/main/java/com/misterdiallo/backend/springswaggerdocumentation/SpringSwaggerDocumentationApplication.java
@@ -0,0 +1,13 @@
+package com.misterdiallo.backend.springswaggerdocumentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwaggerDocumentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwaggerDocumentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misterdiallo/backend/springswaggerdocumentation/SpringSwaggerDocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misterdiallo.backend.springswaggerdocumentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwaggerDocumentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
